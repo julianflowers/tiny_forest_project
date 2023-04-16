79aa148f9d2fb5a69c55b4ebf8a4733d99dc29fc
julianflowers <julianflowers@users.noreply.github.com>
2023-04-16 10:16:05 +0100

    update

--- scripts/tiny_forest_web_scrape.R
+++ scripts/tiny_forest_web_scrape.R
@@ -30,7 +30,7 @@ create_tf_table <- function(df, i){
   unnest("value") |>
   filter(str_detect(value, "m2") |
            str_detect(value, "GPS") |
-           str_detect(value, "^\\d{2}.*20\\d{2}")|
+           str_detect(value, "^\\d{2}\\D.*20\\d{2}")|
            str_detect(value, "Species")) |>
     mutate(tf_id = df$tf_id[i])
 
@@ -38,33 +38,49 @@ create_tf_table <- function(df, i){
 
 tf_table <- map_dfr(1:180, ~(create_tf_table(tf_df, .x)))
 
-tf_table <- tf_table |>
+tf_table_planted <- tf_table |>
   group_by(tf_id) |>
   mutate(n = n(),
-         id = row_number(),
-         metric = case_when(id == 1 ~ "plant date",
+         id = row_number()) |>
+  filter(n > 3) |>
+  mutate(metric = case_when(id == 1 ~ "plant_date",
                             id == 2 ~ "area",
                             id == 3 & n == 5 ~ "class area",
                             id == 3 & n == 4 ~ "trees",
                             id == 4 & n == 5 ~ "trees",
                             TRUE ~ "gps"))
 
-tf_table |>
-  filter(tf_id == 379)
+tf_table_planted |>
+  ungroup() |>
+  mutate(value = case_when(metric == "plant_date" ~ lubridate::dmy() ))
 
-tf_table_tidy <- tf_table |>
-  select(-c(n, id, name)) |>
+
+
+tf_table_tidy <- tf_table_planted |>
+  select(-c(n, name, id)) |>
   pivot_wider(names_from = "metric", values_from = "value", values_fn = list ) |>
   unnest("gps") |>
-  unnest("trees") |>
-  unnest("class area") |>
+  #unnest("class area") |>
   unnest("area") |>
-  unnest("plant date") |>
-  mutate(date = dmy(`plant date`),
+  unnest("plant_date") |>
+  unnest("trees") |>
+  select(-`class area`) |>
+  mutate(date = dmy(plant_date),
          area = parse_number(area),
-         class_area = parse_number(`class area`),
-         trees = str_remove(trees, "Species Planted in the Forest:"))
+         trees = str_remove(trees, "Species Planted in the Forest:"),
+         gps = str_remove(gps, "GPS:")) |>
+  separate(gps, c("lat", 'lon'), sep = ",.") |>
+  mutate(trees = str_split(trees, "\\|"))
 
-tf_table_tidy |>
-  DT::datatable()
+tf_presence_absence <- tf_table_tidy |>
+  ungroup() |>
+  unnest("trees") |>
+  pivot_wider(names_from = "trees", values_from = "trees", values_fill = NA) |>
+  mutate_at(.vars = 7:84, ~(ifelse(is.na(.x), 0, 1))) |>
+  mutate(lat = as.numeric(lat),
+         lon = as.numeric(lon)) |>
+  select(-plant_date) |>
+  janitor::clean_names()
 
+tf_presence_absence |>
+  write_csv("data/tf_trees.csv")
