cae41528ed470847cfa7eeb4cc8b6f9b9c50fa1c
julianflowers <julianflowers@users.noreply.github.com>
2023-04-17 19:22:31 +0100

    updates

--- scripts/nbn_record_counts.R
+++ scripts/nbn_record_counts.R
@@ -10,7 +10,7 @@ nbn_tfb <- map_dfr(c(1:171), ~(tfb_bd_nbn[[.x]] |> mutate(id = tfLL$tf_id[.x])))
   select(1:15) |>
   setDT()
 
-nbn_tfb_class <- nbn_tfb[classs %in% c("Aves", "Insecta", "Magnoliopsida", "Mammalia"),]
+nbn_tfb_class <- nbn_tfb[classs %in% c("Aves", "Insecta", "Magnoliopsida", "Mammalia") & between(year, 2000, 2023),]
 
 
 dt_count(nbn_tfb_class[between(year, 2000, 2023)], classs ) |>
@@ -18,3 +18,11 @@ dt_count(nbn_tfb_class[between(year, 2000, 2023)], classs ) |>
 
 ## top 4 aves, insecta, magnoliopsida, mammalia
 
+
+## create species tables
+
+nbn_aves <- nbn_tfb_class[classs == "Aves", .N, by=.(id, year, species)] |>
+  dt_pivot_wider(names_from = species, values_from = N) |>
+  mutate_all(~(ifelse(is.na(.x), 0, .x)))
+
+View(nbn_aves)
